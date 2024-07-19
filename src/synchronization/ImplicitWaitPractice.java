1ceac2ddccdc9ef12c12d8006dcbb659d8fbb8e7
Srivalli <snc.srivalli1@gmail.com>
2024-07-19 16:34:28 +0530

    implicitly wait added

---
+++ src/synchronization/ImplicitWaitPractice.java
@@ -0,0 +1,22 @@
+package synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWaitPractice {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//driver.findElement(By.linkText("Log in")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Forgot')]")).click();
+		driver.quit();
+	}
+
+}
