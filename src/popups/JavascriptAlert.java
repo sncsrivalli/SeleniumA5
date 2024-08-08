08bafc1a2bafd6d60c2388f5931e21c9cdb37d33
Srivalli <snc.srivalli1@gmail.com>
2024-08-08 17:13:22 +0530

    @@

---
+++ src/popups/JavascriptAlert.java
@@ -0,0 +1,34 @@
+package popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavascriptAlert {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	//	driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		
+	//	driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		Alert a = driver.switchTo().alert();
+		System.out.println(a.getText());
+	//	a.dismiss();
+		a.sendKeys("ABCDEF");
+		a.accept();
+		
+		String result = driver.findElement(By.id("result")).getText();
+		System.out.println(result);
+		driver.quit();
+	}
+
+}
