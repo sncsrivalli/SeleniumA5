1da350faf7b0569e763091186d14081478a6909e
Srivalli <snc.srivalli1@gmail.com>
2024-08-23 16:29:25 +0530

    POM added

---
+++ src/pom/FBLoginPage.java
@@ -0,0 +1,51 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FBLoginPage {
+
+	// Declaration
+	@FindBy(id="email")
+	private WebElement emailTF;
+	
+	@FindBy(name="pass")
+	private WebElement passwordTF;
+	
+	@FindBy(name="login")
+	private WebElement loginBTN;
+	
+	@FindBy(linkText = "Forgotten password?")
+	private WebElement forgotPWDLink;
+	
+	@FindBy(xpath = "//a[text()='Create new account']")
+	private WebElement createAccountLink;
+	
+	// Initialization
+	public FBLoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	// Utilization
+	public void setEmail(String email) {
+		emailTF.sendKeys(email);
+	}
+	
+	public void setPassword(String pwd) {
+		passwordTF.sendKeys(pwd);
+	}
+	
+	public void clickLogin() {
+		loginBTN.click();
+	}
+	
+	public void clickForgotPWD() {
+		forgotPWDLink.click();
+	}
+	
+	public void clickCreateAccount() {
+		createAccountLink.click();
+	}
+}
---
+++ src/pom/SkillraryRegistrationTest.java
@@ -0,0 +1,29 @@
+package pom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SkillraryRegistrationTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoapp.skillrary.com/login.php?type=register");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		SkillraryRegistrationPage register = new SkillraryRegistrationPage(driver);
+		register.setFirstName("ABCD");
+		register.setLastName("XYZ");
+		register.setEmail("abcd_xyz@gmail.com");
+		register.setPassword("abcd@xyz#123");
+		register.setConfirmPassword("abcd@xyz#123");
+		register.clickRegister();
+		
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+}
